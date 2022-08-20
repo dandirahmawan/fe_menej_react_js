60ea052bd05acd538a64c1117579ede4f2c2f891
dandi rahmawan <dandirahmawan95@gmail.com>
2022-08-20 23:01:04 +0700

    project services

---
+++ src/services/project_service.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Fetch from '../function/fetchApi'
+import { getCookieSessionId, getCookieUserId } from '../function/function'
+
+let fetch = new Fetch()
+export default class project_service {
+
+    get_all_project = async () => {
+        let resp = await fetch.getGolang("/project/list")
+        return resp
+    }
+
+}
